a866a8acd34f65347b1c4b5204ae9d94ba798c88
Litorias <maks.test.qa@gmail.com>
2024-08-02 19:10:28 +0300

    Homework 10

--- Assets/Scripts/PlayerMover.cs
+++ Assets/Scripts/PlayerMover.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 
 public class PlayerMover : MonoBehaviour
 {
+    private const float SPEED_COEFFICIENT = 50;
+
     [SerializeField] private Rigidbody2D _rigidbody;
     [SerializeField] private float _speedX = 0;
 
     private void FixedUpdate()
     {
-        _rigidbody.velocity = new Vector2(_speedX, _rigidbody.velocity.y);
+        _rigidbody.velocity = new Vector2(_speedX * SPEED_COEFFICIENT * Time.fixedDeltaTime, _rigidbody.velocity.y);
     }
 }
